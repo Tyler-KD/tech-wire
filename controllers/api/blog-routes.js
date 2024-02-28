cdfedea3d67e7ec3b9ec1fbb10abf20dff359e2e
unknown <tyler.kd.knapp@gmail.com>
2024-02-28 14:13:43 -0600

    Added DELETE route in blog-routes to destroy/delete a blog post

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -20,28 +20,51 @@ router.get('/', async (req, res) => {
         res.status(200).json(blogData);
     } catch (err) {
         // 500 status code means the server encountered an unexpected condition
-        // that prevented it from fulfilling the request
+        // that prevented it from fulfilling the request.
         res.status(500).json(err);
     }
 });
 
 // Route to create/add a blog post using async/await.
 // If user's cookie has not expired, then route is accessed.
-// If user's cookie has expired', then throw 400 Bad Request error, and navigate back to the login route.
+// If user's cookie has expired, then throw 400 Bad Request error, and navigate back to the login route.
 router.post('/', handleExpiredCookie, async (req, res) => {
     try {
         const blogData = await Blog.create({
             ...req.body,
             user_id: req.session.user_id,
         });
-        // If the blog is successfully created, the new response will be returned as json
-        // 200 status code means the request is successful
+        // If the blog is successfully created, the new response will be returned as json.
+        // 200 status code means the request is successful.
         console.log(blogData);
         res.status(200).json(blogData);
 
     } catch (err) {
         // 400 Bad Request response status code indicates that the server cannot or will not process
-        // the request due to something that is perceived to be a client error
+        // the request due to something that is perceived to be a client error.
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+// Route to destroy/delete a blog post using async/await.
+// If user's cookie has not expired, then route is accessed.
+// If user's cookie has expired, then throw 400 Bad Request error, and navigate back to the login route.
+router.delete('/:id', handleExpiredCookie, async (req, res) => {
+    try {
+        const blogData = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        // If the blog is successfully deleted, the new response will be returned as json.
+        // 200 status code means the request is successful.
+        console.log(blogData);
+        res.status(200).json(blogData);
+    } catch (err) {
+        // 400 Bad Request response status code indicates that the server cannot or will not process
+        // the request due to something that is perceived to be a client error.
         console.log(err);
         res.status(400).json(err);
     }
